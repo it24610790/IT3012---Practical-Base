acda575fe8ef3a903e25d27c64274c5ac335eedf
Akini Panapitiya <akinipanapitiya@Akinis-MacBook-Pro.local>
2026-08-03 10:13:03 +0530

    Step 1.3: Implement ModelBasedAgent internal state and loop escaping

--- agent.py
+++ agent.py
@@ -31,11 +31,35 @@ class SimpleReflexAgent:
 
 class ModelBasedAgent:
     def __init__(self):
-        pass
+        # Internal State / keep the Memory
+        self.last_action = None
 
     def sense_and_act(self, percept: dict) -> str:
-        return 'Up'
+        wall_ahead = percept.get('wall_ahead', False)
+        food_here = percept.get('food_here', False)
 
+        # Rule 1: Food is present -> Suck it up
+        if food_here:
+            action = 'suck'
+        
+        # Rule 2: Wall is ahead, use Memory (last_action) to determine next action
+        elif wall_ahead:
+            if self.last_action == 'Left':
+                action = 'Down'
+            elif self.last_action == 'Down':
+                action = 'Right'
+            elif self.last_action == 'Right':
+                action = 'Up'
+            else:
+                action = 'Left'
+        
+        # Rule 3: there is no wall ahead and no food, just move forward (Up)
+        else:
+            action = 'Up'
+
+        # Store Action in Memory
+        self.last_action = action
+        return action
 
 class SearchAgent:
     def __init__(self):
